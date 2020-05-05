7b08817de942e424e92ba9b9ea247ca0589472de
egipratama <egila391@gmail.com>
2020-05-05 22:10:15 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 function App() {
+  useEffect(() => {
+    document.getElementById('init').remove();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CircularProgress />
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
     </div>
   );
 }
